654eadaf07161ec7d3d1be481e86b2db845b84cf
Androz2091 <androz2091@gmail.com>
2021-01-24 21:20:58 +0100

    :sparkles: Use x-www-form-urlencoded

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ const fetch = require('node-fetch')
 
 module.exports = class WhmcsAPI {
 
-    constructor ({ identifier, secret, baseURL }) {
-        this.identifier = identifier || process.env.WHMCS_IDENTIFIER;
-        this.secret = secret || process.env.WHMCS_SECRET;
-        this.baseURL = baseURL || process.env.WHMCS_URL;
+    constructor (options = {}) {
+        this.identifier = options.identifier || process.env.WHMCS_IDENTIFIER;
+        this.secret = options.secret || process.env.WHMCS_SECRET;
+        this.baseURL = options.baseURL || process.env.WHMCS_BASE_URL;
     }
 
 
     request (action, method = 'GET', body) {
-            return new Promise((resolve) => {
+        return new Promise((resolve) => {
+            const params = new URLSearchParams();
+            params.append('action', action);
+            params.append('identifier', this.identifier);
+            params.append('secret', this.secret);
+            Object.keys(body).forEach((key) => params.append(key, body[key]));
             fetch(`${this.baseURL}/includes/api.php`, {
-                action,
                 method,
-                body
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: params.toString()
             }).then((res) => {
-                res.json().then((data) => {
-                    resolve(data);
+                res.text().then((data) => {
+                    try {
+                        resolve(JSON.parse(data));
+                    } catch (e) {
+                        throw new Error(`Invalid response: ${data}`);
+                    }
                 });
             });
         });
